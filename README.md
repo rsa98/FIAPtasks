5239f71b3bd5ff267006ce7be3c5863b09543f94
rsa98 <rafaellasda.1998@gmail.com>
2022-02-25 23:19:08 -0300

    Configurando a segunda tela

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FIAPtasks
+
+Projeto em desenvolvimento para a tarefa de Layouts Complexos
+
+Até o momento contendo a tela de login e a segunda tela (tela principal)
+
+:construction: Projeto em construção :construction:`:construction:```
--- app/src/main/java/br/com/empresa/fiapenterpriseconnection/MainActivity.kt
+++ app/src/main/java/br/com/empresa/fiapenterpriseconnection/MainActivity.kt
@@ -1,11 +1,23 @@
 package br.com.empresa.fiapenterpriseconnection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+     val button: Button = findViewById(R.id.entrarButton)
+        button.setOnClickListener{
+
+            IrParaSegundaTela()
+        }
+    }
+    private fun IrParaSegundaTela(){
+
+        val segundaTela = Intent(this,Tela2Activity::class.java)
+        startActivity(segundaTela)
     }
 }
