e71bc082c5be6a169efd0af1ad0db3f353050612
rsa98 <rafaellasda.1998@gmail.com>
2022-02-28 19:35:32 -0300

    Configurando a segunda tela

---
+++ app/src/main/java/br/com/empresa/fiapenterpriseconnection/Tela2Activity.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.fiapenterpriseconnection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
